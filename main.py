9cd6f16f510d72e58d3c9252870edd2d6f3d6fda
Sergio Roque Hernández <sergioroquehernandez93@gmail.com>
2025-01-01 11:13:40 +0000

    modified:   main.py 	new file:   proyecto 1.txt 	new file:   sales_data.csv

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+# Carga el dataset en un DataFrame de pandas y muestra las primeras 10 filas
+df = pd.read_csv("sales_data.csv")
+print(df.head(10))
+
+# Obtén un resumen de las columnas, tipos de datos y valores nulos
+print(df.info())
+print(df.isnull().sum())
+
+# Cuenta el número total de filas y columnas en el dataset
