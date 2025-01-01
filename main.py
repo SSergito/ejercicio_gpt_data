d048278661444f61b9c5c1b34b678076c1561287
Sergio Roque Hernández <sergioroquehernandez93@gmail.com>
2025-01-01 11:45:14 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -13,3 +13,16 @@ rows, columns = df.shape
 print(f"Número de filas: {rows}\nNúmero de columnas: {columns}")
 
 # Filtra las transacciones para un producto específico
+df_watch = df.loc[df["product"] == "Smartwatch"]
+print(df_watch)
+
+# Ordena las transacciones por fecha
+print(df.sort_values(by="date",ascending=True))
+
+# Calcula el precio promedio de los productos vendidos.
+print(f"Precio promedio de ventas: {df["price"].mean():.2f}")
+##### Extra a la pregunta quiero hallar el precio promedio por cada producto
+precios_promedio = df.groupby("product")["price"].mean()
+
+for producto, promedio in precios_promedio.items():
+    print(f"Precio promedio de {producto}: {promedio:.2f}")
